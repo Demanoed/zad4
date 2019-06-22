38d2df848c7d3fdd947ef39697619c5e1df950d8
Трефилов Дмитрий <trefilowdem@mail.ru>
2019-06-22 11:08:41 +0500

    Add function CreateComplexNum

--- ComplexNum/ComplexNum/Program.cs
+++ ComplexNum/ComplexNum/Program.cs
@@ -9,9 +9,32 @@ namespace ComplexNum
 {
     class Program
     {
+        private static Complex u, v, w;
+        private static void CreateComplexNum()
+        {
+            double u1, u2, v1, v2, w1, w2;
+            ColorMess.Yellow("\n Введите целую часть комплексного числа u: ");
+            u1 = Input.Check(double.MinValue, double.MaxValue);
+            ColorMess.Yellow("\n Введите мнимую часть комплексного числа u: ");
+            u2 = Input.Check(double.MinValue, double.MaxValue);
+            Console.Clear();
+            ColorMess.Yellow("\n Введите целую часть комплексного числа v: ");
+            v1 = Input.Check(double.MinValue, double.MaxValue);
+            ColorMess.Yellow("\n Введите мнимую часть комплексного числа v: ");
+            v2 = Input.Check(double.MinValue, double.MaxValue);
+            Console.Clear();
+            ColorMess.Yellow("\n Введите целую часть комплексного числа w: ");
+            w1 = Input.Check(double.MinValue, double.MaxValue);
+            ColorMess.Yellow("\n Введите мнимую часть комплексного числа w: ");
+            w2 = Input.Check(double.MinValue, double.MaxValue);
+            Console.Clear();
+            u = new Complex(u1, u2);
+            v = new Complex(v1, v2);
+            w = new Complex(w1, w2);
+        }
         static void Main()
         {
-
+            CreateComplexNum();
         }
     }
 }
