cda046892d5307eb271fce67076bce7959725d28
Трефилов Дмитрий <trefilowdem@mail.ru>
2019-06-22 13:46:34 +0500

    Add function Formula

--- ComplexNum/ComplexNum/Program.cs
+++ ComplexNum/ComplexNum/Program.cs
@@ -9,7 +9,7 @@ namespace ComplexNum
 {
     class Program
     {
-        private static Complex u, v, w;
+        private static Complex u, v, w, res;
         private static void CreateComplexNum()
         {
             double u1, u2, v1, v2, w1, w2;
@@ -32,10 +32,17 @@ namespace ComplexNum
             v = new Complex(v1, v2);
             w = new Complex(w1, w2);
         }
+        private static Complex Formula()
+        {
+            Complex res = 2*u+(3*u*w)/(2+w-u)-7;
+            return new Complex(res);
+        }
         static void Main()
         {
             CreateComplexNum();
-            
+            res = Formula();
+            ColorMess.Cyan("\n Результат вычисления формулы равен: "+res);
+            Message.GoToBack();
         }
     }
 }
