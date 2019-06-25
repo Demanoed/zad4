fb7eb74e095d81ef5b2eda95c66c254c9ada5b90
Дмитрий Трефилов <trefilowdem@mail.ru>
2019-06-26 00:30:38 +0500

    Add comments

--- ComplexNum/ComplexNum/Complex.cs
+++ ComplexNum/ComplexNum/Complex.cs
@@ -8,60 +8,56 @@ namespace ComplexNum
 {
     class Complex
     {
+        #region Переменные для целой и мнимой части
         private double m_real;
         private double m_imag;
-
+        #endregion
         #region Конструкторы
-
         public Complex(double re)
         {
             m_real = re;
             m_imag = 0.0;
         }
-
         public Complex(double re, double im)
         {
             m_real = re;
             m_imag = im;
         }
-
         public Complex(Complex x)
         {
             m_real = x.Real;
             m_imag = x.Imag;
         }
         #endregion
-
+        #region Свойства 
         public double Real
         {
             get { return m_real; }
             set { m_real = value; }
         }
-
         public double Imag
         {
             get { return m_imag; }
             set { m_imag = value; }
         }
-
+        #endregion
+        #region Доп функции
         public double Abs
         {
             get { return Math.Sqrt(m_imag * m_imag + m_real * m_real); }
         }
-
         public double Arg
         {
             get { return Math.Atan(m_imag / m_real); }
         }
-
-        /// <summary>
-        /// Получить комплексно-сопряженное число
-        /// </summary>
+        #endregion
+        #region Получение комплексно-сопряженного числа
         public Complex GetConjugate()
         {
             return new Complex(m_real, -m_imag);
         }
-
+        #endregion
+        #region Преобразование в строку
         public override string ToString()
         {
             string res = "";
@@ -80,65 +76,52 @@ namespace ComplexNum
 
                 res += m_imag.ToString() + "i";
             }
-
             return res;
         }
-
+        #endregion
         #region Перегруженные операторы сложения
         public static Complex operator +(Complex c1, Complex c2)
         {
             return new Complex(c1.Real + c2.Real, c1.Imag + c2.Imag);
         }
-
         public static Complex operator +(Complex c1, double c2)
         {
             return new Complex(c1.Real + c2, c1.Imag);
         }
-
         public static Complex operator +(double c1, Complex c2)
         {
             return new Complex(c1 + c2.Real, c2.Imag);
         }
         #endregion
-
-
         #region Перегруженные операторы вычитания
         public static Complex operator -(Complex c1, Complex c2)
         {
             return new Complex(c1.Real - c2.Real, c1.Imag - c2.Imag);
         }
-
         public static Complex operator -(Complex c1, double c2)
         {
             return new Complex(c1.Real - c2, c1.Imag);
         }
-
         public static Complex operator -(double c1, Complex c2)
         {
             return new Complex(c1 - c2.Real, -c2.Imag);
         }
         #endregion
-
-
         #region Перегруженные операторы умножения
         public static Complex operator *(Complex c1, Complex c2)
         {
             return new Complex(c1.Real * c2.Real - c1.Imag * c2.Imag,
                 c1.Real * c2.Imag + c1.Imag * c2.Real);
         }
-
         public static Complex operator *(Complex c1, double c2)
         {
             return new Complex(c1.Real * c2, c1.Imag * c2);
         }
-
         public static Complex operator *(double c1, Complex c2)
         {
             return new Complex(c1 * c2.Real, c1 * c2.Imag);
         }
         #endregion
-
-
         #region Перегруженные операторы деления
         public static Complex operator /(Complex c1, Complex c2)
         {
@@ -146,29 +129,29 @@ namespace ComplexNum
             return new Complex((c1.Real * c2.Real + c1.Imag * c2.Imag) / Denominator,
                 (c2.Real * c1.Imag - c2.Imag * c1.Real) / Denominator);
         }
-
         public static Complex operator /(Complex c1, double c2)
         {
             return new Complex(c1.Real / c2, c1.Imag / c2);
         }
-
         public static Complex operator /(double c1, Complex c2)
         {
             double Denominator = c2.Real * c2.Real + c2.Imag * c2.Imag;
             return new Complex((c1 * c2.Real) / Denominator, (-c2.Imag * c1) / Denominator);
         }
         #endregion
-
+        #region Перегруженный оператор ==
         public static bool operator ==(Complex c1, Complex c2)
         {
             return c1.Real == c2.Real && c1.Imag == c2.Imag;
         }
-
+        #endregion
+        #region Перегруженный оператор !=
         public static bool operator !=(Complex c1, Complex c2)
         {
             return c1.Real != c2.Real || c1.Imag != c2.Imag;
         }
-
+        #endregion
+        #region Перегруженный Equals
         public override bool Equals(object obj)
         {
             if (obj == null || obj.GetType() != this.GetType())
@@ -178,10 +161,12 @@ namespace ComplexNum
 
             return this == (Complex)obj;
         }
-
+        #endregion
+        #region Перегруженное получение хэш-кода
         public override int GetHashCode()
         {
             return m_real.GetHashCode() + m_imag.GetHashCode();
         }
+        #endregion
     }
 }
--- ComplexNum/ComplexNum/Program.cs
+++ ComplexNum/ComplexNum/Program.cs
@@ -9,7 +9,10 @@ namespace ComplexNum
 {
     class Program
     {
+        #region Комплексные числа
         private static Complex u, v, w, res;
+        #endregion
+        #region Ввод значений
         private static void CreateComplexNum()
         {
             double u1, u2, v1, v2, w1, w2;
@@ -32,11 +35,15 @@ namespace ComplexNum
             v = new Complex(v1, v2);
             w = new Complex(w1, w2);
         }
+        #endregion
+        #region Возвращает результат вычисления формулы
         private static Complex Formula()
         {
             Complex res = 2*u+(3*u*w)/(2+w-u)-7;
             return new Complex(res);
         }
+        #endregion
+        #region Для ввода и вывода результата вычисления
         static void Main()
         {
             CreateComplexNum();
@@ -44,5 +51,6 @@ namespace ComplexNum
             ColorMess.Cyan("\n Результат вычисления формулы равен: "+res);
             Message.GoToBack();
         }
+        #endregion
     }
 }
